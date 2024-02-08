84f437fb6d48664c6b55a5aa83e11c0863b28f65
Yusuf Osama <yosef.eldesoky@gmail.com>
2024-02-08 21:15:37 +0700

    added a Post model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('post', ['post' => Post::all()]);
+});
+
+
+Route::get('posts/{post}', function ($id) {
+    return view('post', ['post' => Post::find($id)]);
 });
